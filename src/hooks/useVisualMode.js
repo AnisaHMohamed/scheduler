8127c44dc47c97d7f56d06a2a8f282987eb48474
Anisa <anisahamohamed@gmail.com>
2020-01-26 01:25:41 -0500

    aadd transition and back functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,42 @@
 import { useState, useCallback } from 'react'
+  const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  
-function useVisualMode (mode) {
-  const [visualMode, setVisualMode] = useState(mode)
-  return {mode}
+  const transition = (newMode, replace) => {
+    if (!replace) {
+      setHistory(prev => ([...prev, mode]))
+    }
+    setMode(newMode);
+  };
+
+  const back = () => {
+    if (history.length === 1) {
+      return;
+    }
+    let historyArray = history.slice(0, history.length - 1)
+    setHistory([...historyArray])
+    setMode(history[history.length - 2])
+  };
+
+
+  return { mode, transition, back };
 }
 
+  export {useVisualMode}
+  
+  
+  
 
-//  return an object with a property mode
+  
+  
+// Create a transition function within useVisualMode that will take in a new mode and update the mode state with the new value.
 
-export default useVisualMode
+
+// function useCustomHook() {
+//   function action() {}
+
+//   return { action };
+// }
+
+
